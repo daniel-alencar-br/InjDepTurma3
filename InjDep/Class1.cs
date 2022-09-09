25903bae03b6e84a230868fd106c49353fb7d560
daniel-alencar-br <danielalencar.br@gmail.com>
2022-09-08 22:46:42 -0300

    Adicionar arquivos de projeto.

---
+++ InjDep/Class1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InjDep
+{
+    // Nivel 1 - UMA GRANDE CLASSE 
+    // Nivel 2 - Varias classes dependendo umas das outras
+    //           Alto Acoplamento
+    // Nivel 3 - Injeção de Dependencia / Baixo Acoplamento
+    
+    public interface IBanco
+    {
+        void Conectar();
+        void EnviarPix();
+    }
+
+    public class Bradesco : IBanco
+    {
+        public void Conectar()
+        {
+            //
+            // Comp segurança
+            //
+        }
+        public void EnviarPix()
+        {
+            //
+        }
+    }
+
+    public class Santander : IBanco
+    {
+        public void Conectar()
+        {
+            //
+            // Token certificado digital fisico
+            //
+        }
+        public void EnviarPix()
+        {
+            //
+        }
+    }
+
+    public class Pagamentos
+    {
+        IBanco MeuBanco;
+
+        // Constructor Dependency Injection
+        public Pagamentos(IBanco A)
+        {
+            MeuBanco = A;
+        }
+
+        public void RealizarTransferencia()
+        {
+            MeuBanco.Conectar();
+            MeuBanco.EnviarPix();
+        }
+
+        // Method Injection Dependency
+        public void PagarConta(IBanco Algo)
+        {
+            Algo.Conectar();
+        }
+    }
+    
+   
+
+    public class Sistema
+    {
+        public void ModuloFinanceiro()
+        {
+            Bradesco MeuBanco = new Bradesco();
+            Santander BancoPJ = new Santander();
+
+            Pagamentos ContaPessoal = new Pagamentos(MeuBanco);
+            ContaPessoal.RealizarTransferencia();
+
+            Pagamentos ContaEmpresa = new Pagamentos(BancoPJ);
+            ContaEmpresa.RealizarTransferencia();
+
+            ContaEmpresa.PagarConta(BancoPJ);
+        }
+    }
+    }
+
+
